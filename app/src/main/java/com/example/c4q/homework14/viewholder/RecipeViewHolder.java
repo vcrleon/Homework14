b2992c934ad81a6127ed246dfb372881b1dd216c
Victoria Leon <vcrleon@gmail.com>
2018-01-19 00:15:41 -0500

    implemented recycler view to display recipe image and recipe name in main activity

---
+++ app/src/main/java/com/example/c4q/homework14/viewholder/RecipeViewHolder.java
@@ -0,0 +1,36 @@
+package com.example.c4q.homework14.viewholder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.c4q.homework14.R;
+import com.example.c4q.homework14.model.Matches;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by c4q on 1/15/18.
+ */
+
+public class RecipeViewHolder extends RecyclerView.ViewHolder {
+    private ImageView foodImage;
+    private TextView recipeName;
+
+    public RecipeViewHolder(View itemView) {
+        super(itemView);
+        foodImage = itemView.findViewById(R.id.food_image_view);
+        recipeName = itemView.findViewById(R.id.recipe_name_view);
+    }
+
+    public void onBind(Matches recipe) {
+        Picasso.with(itemView.getContext())
+                .load(recipe.getSmallImageUrls().get(0))
+                .into(foodImage);
+
+        recipeName.setText(recipe.getRecipeName());
+
+    }
+
+
+}
